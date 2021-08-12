ddeeea13b605ac20dc68fd3ec0fb43d8c31cfad8
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-12 19:00:43 +0200

    Header component

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,19 @@
 import Nav from "./Nav";
 import styles from "../styles/Layout.module.css";
+import Header from "./Header";
+import React from "react";
 
 const Layout = ({ children }) => {
     return (
-        <>
-        <Nav/>
-        <div className={styles.container}>
-            <main className={styles.main}>{children}</main>
-        </div>
-        </>
+        <React.Fragment>
+            <Nav />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <Header />
+                    {children}
+                </main>
+            </div>
+        </React.Fragment>
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
     return (
         <div>
+            <Head>
+                <title>WebDev News</title>
+            </Head>
             <h1>App</h1>
         </div>
     );
